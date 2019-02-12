d47839bbbfeffe1421d691b1cde580cf2c59bb4d
liulichao <liulc@idss-cn.com>
2019-02-12 14:26:20 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div class="hello" id="vue_det">
+    <h1>{{ msg }}</h1>
+    <!--<h1>site : {{site}}</h1>-->
+    <!--<h1>url : {{url}}</h1>-->
+    <!--<h1>{{details()}}</h1>-->
   </div>
 </template>
 
 <script>
-export default {
-  name: 'App'
-}
+  export default {
+    id: 'vue_det',
+    data () {
+      return {
+        msg: '欢迎来到菜鸟教程！'
+      }
+    }
+  }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
