3270a39a88200b5c7498ead259afaa51bba35031
liulc <liulc@idss-cn.com>
2020-04-13 17:27:22 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <div class="hello" id="vue_det">
-    <h1>{{ msg }}</h1>
-    <!--<h1>site : {{site}}</h1>-->
-    <!--<h1>url : {{url}}</h1>-->
-    <!--<h1>{{details()}}</h1>-->
+  <div id="app">
+    <router-view/>
   </div>
 </template>
 
 <script>
   export default {
-    id: 'vue_det',
-    data () {
-      return {
-        msg: '欢迎来到菜鸟教程！'
-      }
-    }
+    name: 'App'
   }
 </script>
+
+<style>
+</style>
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id = 'vue_det'>
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "Home",
+        id: 'vue_det',
+        data () {
+          return {
+            msg: '欢迎来到菜鸟教程！'
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
